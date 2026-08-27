06fc7b69ee950f51c5ce426a19fae58b964bfbc3
iamYogeshkumar <iyogeshsingh.9+gitHub@gmail.com>
2026-08-27 16:32:45 +0530

    exist category implemented

--- src/main/java/com/enotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.enotes.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,6 +29,17 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(exception.getError(),HttpStatus.BAD_REQUEST);
 	}
 	
+	@ExceptionHandler(ExistDataException.class)
+	public ResponseEntity<?> handleExistDataException(ExistDataException exception){
+		log.error("GlobalExceptionHandler :: handleResourceNotFoundException "+exception.getMessage());
+		return new ResponseEntity<>(exception.getMessage(),HttpStatus.CONFLICT);
+	}
 	
-	 
+//	HttpMessageNotReadableException
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handlHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+		log.error("GlobalExceptionHandler :: handleResourceNotFoundException "+exception.getMessage());
+		return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
+	}
 }
